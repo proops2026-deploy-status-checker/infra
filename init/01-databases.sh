76b5ba0a7e66db8b2d4f6dfac4a1a68b7ff26fcb
Tien Nguyen <thaotien622052@gmail.com>
2026-09-08 22:13:34 +0700

    Harden database user isolation

--- init/01-databases.sh
+++ init/01-databases.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql \
+  -v ON_ERROR_STOP=1 \
+  --username "$POSTGRES_USER" \
+  --set=deploy_db_password="$DEPLOY_DB_PASSWORD" \
+  --set=log_db_password="$LOG_DB_PASSWORD" <<'EOSQL'
   CREATE DATABASE deploy_db;
   CREATE DATABASE log_db;
 
-  CREATE USER deploy_user WITH PASSWORD '${DEPLOY_DB_PASSWORD}';
-  CREATE USER log_user WITH PASSWORD '${LOG_DB_PASSWORD}';
+  CREATE USER deploy_user WITH PASSWORD :'deploy_db_password';
+  CREATE USER log_user WITH PASSWORD :'log_db_password';
 
-  REVOKE CONNECT ON DATABASE deploy_db FROM PUBLIC;
-  REVOKE CONNECT ON DATABASE log_db FROM PUBLIC;
+  REVOKE ALL ON DATABASE deploy_db FROM PUBLIC;
+  REVOKE ALL ON DATABASE log_db FROM PUBLIC;
 
-  GRANT ALL PRIVILEGES ON DATABASE deploy_db TO deploy_user;
-  GRANT ALL PRIVILEGES ON DATABASE log_db TO log_user;
+  GRANT CONNECT ON DATABASE deploy_db TO deploy_user;
+  GRANT CONNECT ON DATABASE log_db TO log_user;
+  REVOKE CONNECT ON DATABASE log_db FROM deploy_user;
+  REVOKE CONNECT ON DATABASE deploy_db FROM log_user;
 
   \c deploy_db
-  GRANT ALL ON SCHEMA public TO deploy_user;
+  REVOKE ALL ON SCHEMA public FROM PUBLIC;
+  GRANT USAGE, CREATE ON SCHEMA public TO deploy_user;
 
   \c log_db
-  GRANT ALL ON SCHEMA public TO log_user;
+  REVOKE ALL ON SCHEMA public FROM PUBLIC;
+  GRANT USAGE, CREATE ON SCHEMA public TO log_user;
 EOSQL
